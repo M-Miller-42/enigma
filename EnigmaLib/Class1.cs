9027adc2e1b682235f2961b521a53539a07e93d4
Martin Miller <martin.mil@web.de>
2024-02-03 19:41:54 +0100

    Setup empty project structures

---
+++ EnigmaLib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace EnigmaLib;
+public class Class1
+{
+
+}
---
+++ EnigmaTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EnigmaTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        
+    }
+}
