21ff2d7c76d5dd11023a44bb603df8c2a2d42064
Martin Miller <martin.mil@web.de>
2024-03-01 23:38:49 +0100

    Add tests for permutations and its derived classes

---
+++ EnigmaTests/PermutationTest.cs
@@ -0,0 +1,32 @@
+namespace EnigmaTests;
+
+[TestClass]
+public class PermutationTest
+{
+
+    [TestMethod]
+    public void TestIdentity()
+    {
+        var expected = Enumerable.Range(0, Enigma.N);
+        var actual = Enumerable.Range(0, Enigma.N).Select(Permutation.Identity.ApplyTo);
+        CollectionAssert.AreEqual(expected.ToArray(), actual.ToArray());
+    }
+
+    [TestMethod]
+    public void TestInverse()
+    {
+        var permutation = Permutation.Random();
+        var expected = Enumerable.Range(0, Enigma.N);
+        var actual = expected.Select(i => permutation.ApplyInverseTo(permutation.ApplyTo(i)));
+        CollectionAssert.AreEqual(expected.ToArray(), actual.ToArray());
+    }
+
+    [TestMethod]
+    public void TestInvolution()
+    {
+        var involution = Involution.Random();
+        var expected = Enumerable.Range(0, Enigma.N);
+        var actual = expected.Select(i => involution.ApplyTo(involution.ApplyTo(i)));
+        CollectionAssert.AreEqual(expected.ToArray(), actual.ToArray());
+    }
+}
